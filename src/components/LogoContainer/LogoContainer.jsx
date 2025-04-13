128d602c782a3638af4cc616302ad73add886e51
ionatan_21 <barriosjonatan1221@gmail.com>
2025-04-12 21:34:37 -0600

    new-logo-rs

--- src/components/LogoContainer/LogoContainer.jsx
+++ src/components/LogoContainer/LogoContainer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function LogoContainer() {
   return (
     <img
-      src="/B-link-logo.png"
+      src="/B-link-logo.webp"
       alt="Logo"
       className="z-10 w-3xs h-36 object-cover list-image-none"
     ></img>
