bfd36b2113c0b822bf1d2634c094b0a28a23d587
Jonatan Barrios <barriosjonatan1221@gmail.com>
2026-02-21 23:35:16 -0600

    new redirect

--- src/components/Redirect/RedirectPage.jsx
+++ src/components/Redirect/RedirectPage.jsx
@@ -1,36 +1,19 @@
 import { useEffect } from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { API_ENDPOINTS } from "@/config/api";
 
 const RedirectPage = () => {
   const { slug } = useParams();
-  const navigate = useNavigate();
 
- useEffect(() => {
-    const fetchUrl = async () => {
-      try {
-        const response = await fetch(API_ENDPOINTS.getUrl(slug));
+  useEffect(() => {
+    window.location.href = API_ENDPOINTS.getUrl(slug);
+  }, [slug]);
 
-        if (response.status === 429) {
-          navigate("/429");
-          return;
-        }
-
-        if (!response.ok) throw new Error("URL not found");
-
-        const data = await response.json();
-        window.location.href = data.url_original;
-
-      } catch (error) {
-        console.error("Error fetching URL:", error);
-        navigate("/404"); // Redirigir a una página de error si falla
-      }
-    };
-
-    fetchUrl();
-  }, [slug, navigate]);
-
-  return <p className="w-screen animate-fade-in-down text-center z-10 text-white text-4xl">Redirecting...</p>;
+  return (
+    <p className="w-screen animate-fade-in-down text-center z-10 text-white text-4xl">
+      Redirecting...
+    </p>
+  );
 };
 
 export default RedirectPage;
