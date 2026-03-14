49ebcbd9368a5292fad01334d8680c653ff47e1b
Jonatan Barrios <barriosjonatan1221@gmail.com>
2026-03-14 06:03:58 -0600

    logo mobile fix

--- src/components/LogoContainer/LogoContainer.jsx
+++ src/components/LogoContainer/LogoContainer.jsx
@@ -1,11 +1,9 @@
-import React from "react";
-
 export default function LogoContainer() {
   return (
     <img
-      src="/BLINK.svg"
+      src="/BLINK.webp"
       alt="Logo"
-      className="z-10 w-3xs h-36 object-contain list-image-none animate-fade-in-down"
+      className="z-10 w-3xs bg-transparent h-36 object-contain list-image-none animate-fade-in-down transform-[translateZ(0)]"
     ></img>
   );
 }
